a84ced6a1644d126bbf36f6708ee01ecd356e52d
Turner Naef <Turner.Naef@gmail.com>
2023-04-25 16:15:09 -0600

    Create route basic

--- src/js/trip/trip.routes.js
+++ src/js/trip/trip.routes.js
@@ -13,7 +13,25 @@ const db = require('./tripQueries');
 // *****************************************************
 
 
+app.post('/createTrip', (req, res) => {
+    if (!req.session.user) {
+        return res.status(400).render('pages/login', {
+          message: "Log in to create a trip!"
+        });
+    }
 
+    if (!req.body.departing || !req.body.destination || !req.body.time || !req.body.seats || !req.body.purpose) {
+        return res.status(400).render('pages/my_trips', {
+          message: "Please fill out all fields!",
+          apikey: process.env.JUNNG_KIM_GOOGLE_MAP_API
+        });
+    }
+
+    return res.status(200).render('pages/my_trips', {
+        message: "Trip created!",
+        apikey: process.env.JUNNG_KIM_GOOGLE_MAP_API
+    })
+});
 
 
 module.exports = app;
