89c75c105ccdc7487fe9d36e152381c50984bc87
Jack Paulman <paulman@the18.com>
2023-04-25 15:19:45 -0600

    updated table

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -6,3 +6,15 @@ CREATE TABLE users(
     home_address VARCHAR(100),
     phone VARCHAR(100)
 );
+
+DROP TABLE IF EXISTS trips CASCADE;
+CREATE TABLE trips(
+    trips_id SERIAL PRIMARY KEY NOT NULL,
+    user_id SERIAL NOT NULL,
+    departing CHAR(60) NOT NULL,
+    destination CHAR(60) NOT NULL,
+    purpose VARCHAR(200),
+    time DATE NOT NULL,
+    seats INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
