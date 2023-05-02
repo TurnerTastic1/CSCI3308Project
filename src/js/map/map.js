8cbafccc346109ff9bfe369c799dd9ce571f2bce
Jusung Kim <itmekjs@gmail.com>
2023-05-02 16:49:21 -0600

    showing distance matrix

--- src/js/map/map.js
+++ src/js/map/map.js
@@ -1,99 +1,73 @@
-// let googleMap;
-
-// async function initMap() {
-//   //@ts-ignore
-//   const { Map } = await google.maps.importLibrary("maps");
-  
-//   googleMap = new Map(document.createElement('div'), {
-//     center: { lat: 39.7392, lng: -104.9903 },
-//     zoom: 10,
-//   });
-// }
-
-// initMap();
 
 
-//         google.maps.event.addDomListener(window, 'load', initialize);
+// set map option
 
-//         function initialize() {
+var myLatLng = { lat: 39.7392, lng: -104.9903 };
+var mapOptions = {
+    center: myLatLng,
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
 
-//           var input = document.getElementById('destination');
-//           var input2 = document.getElementById('fromYourLocation');
+// create Map
 
-//           var autocomplete = new google.maps.places.Autocomplete(input);
-//           var autocomplete2 = new google.maps.places.Autocomplete(input2);
-//           autocomplete.addListener('place_changed', function () {
+var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 
-//             var place = autocomplete.getPlace();
+// create a Directions service object to use the route method and get a result for our request
 
-//             // place variable will have all the information you are looking for.
+var directionsService = new google.map.DirectionsService();
 
-//             $('#lat').val(place.geometry['location'].lat());
+// create a DirectionRenderer object which we will use to display
 
-//             $('#long').val(place.geometry['location'].lng());
+var DirectionsDisplay = new google.maps.DirectionsRenderer();
 
-//             autocomplete2.addListener('source_changed'), function () {
+//bind the directionsRenderer to the map
 
-//               var place2 = autocomplete.getPlace();
+DirectionsDisplay.setMap(map);
 
-//               // place variable will have all the information you are looking for.
+// function
 
-//               $('#lat').val(place2.geometry['location'].lat());
+function calRoute() {
+//   //create request
+  var request = {
+      origin: document.getElementById("from").value,
+      destination: document.getElementById("to").value,
+      travelMode: google.maps.TravelMode.DRIVING, // WALKING, BYCYCLING, TRANSFORTATION
+      unitSystem: google.maps.UnitSystem.IMPERIAL
+  }
 
-//               $('#long').val(place2.geometry['location'].lng());
+//   //Pass the request to the route method
+  directionsService.route(request, (result, status) => {
+    if (status == google.maps.DirectionsStatus.Ok) {
 
+      //get distance and time
+      const output = document.querySelector('#output');
+      output.innerHTML = "<div class='alert-info'> From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ". <br /> Driving distance <i class='fa-solid fa-road'></i>: " + result.routes[0].legs[0].distance.text + ". </div>";
 
-              
-//             }
+      //display route
+      directionsDisplay.setDirection(result); 
+    } else {
+      //delete route from map
+      directionsDisplay.setDirection({ routes: []});
 
-//           });
+      //center map in boulder
+      map.setCenter(myLatLng);
 
-//         }
+      //show error message.
+      output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance. </div>"
 
+    }
+  });
+}
 
-// // set map option
+// create autocomplete object
 
-// var myLatLng = { lat: 39.7392, lng: -104.9903 };
-// var mapOptions = {
-//     center: myLatLng,
-//     zoom: 10,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-// };
+var options = {
+  types: ['(cities)']
+}
 
-// // create Map
+var input1 = document.getElementById("from");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options)
 
-// var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-
-// // create a Directions service object to use the route method and get a result for our request
-
-// var directionsService = new google.map.DirectionsService();
-
-// // create a DirectionRenderer object which we will use to display
-
-// var DirectionsDisplay = new google.maps.DirectionsRenderer();
-
-// //bind the directionsRenderer to the map
-
-// DirectionsDisplay.setMap(map);
-
-// // function
-
-// function calRoute() {
-// //   //create request
-//   var request = {
-//       origin: document.getElementById("from").value,
-//       destination: document.getElementById("to").value,
-//       travelMode: google.maps.TravelMode.DRIVING, // WALKING, BYCYCLING, TRANSFORTATION
-//       unitSystem: google.maps.UnitSystem.IMPERIAL
-//   }
-
-// //   //Pass the request to the route method
-//   directionsService.route(request, (result, status) => {
-//     if (status == google.maps.DirectionsStatus.Ok) {
-
-//       //get distance and time
-//       const output = document.querySelector('#output');
-//       output.innerHTML = "<div class='alert-info'> From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ". <br /> Driving distance <i class='fa-solid fa-road'></i>:" + result.routes[0].legs[0].distance.text + ".<br />Duration "
-//     }
-//   })
-// }
+var input2 = document.getElementById("to");
+var autocomplete2 = new google.maps.places.Autocomplete(input2. options)
